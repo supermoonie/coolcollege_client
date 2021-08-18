4ab4aea7d016dc5ba58705e7475168a8d6413c0f
super_w <wangchao_moonie@163.com>
2021-08-18 23:08:20 +0800

    cool college

--- src/main/java/com/github/supermoonie/coolcollege/router/DownloadRouter.java
+++ src/main/java/com/github/supermoonie/coolcollege/router/DownloadRouter.java
@@ -3,16 +3,31 @@ package com.github.supermoonie.coolcollege.router;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.github.supermoonie.coolcollege.App;
+import com.github.supermoonie.coolcollege.httpclient.CustomHttpClient;
 import com.github.supermoonie.coolcollege.router.req.DownloadReq;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.RequestBuilder;
 import org.cef.browser.CefBrowser;
 import org.cef.browser.CefFrame;
 import org.cef.browser.CefMessageRouter;
 import org.cef.callback.CefQueryCallback;
 import org.cef.handler.CefMessageRouterHandlerAdapter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author supermoonie
@@ -21,7 +36,10 @@ import java.util.List;
 @Slf4j
 public class DownloadRouter extends CefMessageRouterHandlerAdapter {
 
+    private static final Map<String, Integer> DOWNLOADING_MAP = new ConcurrentHashMap<>();
+
     private static final String DOWNLOAD_REQ = "DOWNLOAD_REQ:";
+    private static final String DOWNLOADING_QUERY = "DOWNLOADING_QUERY:";
 
     @Getter
     private final CefMessageRouter router;
@@ -48,6 +66,8 @@ public class DownloadRouter extends CefMessageRouterHandlerAdapter {
         try {
             if (request.startsWith(DOWNLOAD_REQ)) {
                 onDownloadReq(request, callback);
+            } else if (request.startsWith(DOWNLOADING_QUERY)) {
+                onDownloadingQuery(request, callback);
             } else {
                 callback.failure(404, "no cmd found");
                 return false;
@@ -60,11 +80,65 @@ public class DownloadRouter extends CefMessageRouterHandlerAdapter {
         }
     }
 
+    private void onDownloadingQuery(String request, CefQueryCallback callback) {
+        App.getInstance().getExecutor().execute(() -> {
+            String reqText = request.substring(DOWNLOADING_QUERY.length());
+            List<String> idList = JSON.parseObject(reqText, new TypeReference<List<String>>(){});
+            Map<String, Integer> map = new HashMap<>();
+            for (String id : idList) {
+                Integer progress = DOWNLOADING_MAP.getOrDefault(id, -2);
+                map.put(id, progress);
+            }
+            callback.success(JSON.toJSONString(map));
+        });
+    }
+
     private void onDownloadReq(String request, CefQueryCallback callback) {
         App.getInstance().getExecutor().execute(() -> {
             String reqText = request.substring(DOWNLOAD_REQ.length());
             log.info("reqText: {}", reqText);
-            List<DownloadReq> downloadReqList = JSON.parseObject(reqText, new TypeReference<List<DownloadReq>>(){});
+            List<DownloadReq> downloadReqList = JSON.parseObject(reqText, new TypeReference<List<DownloadReq>>() {
+            });
+            final CustomHttpClient httpClient = new CustomHttpClient();
+            for (final DownloadReq req : downloadReqList) {
+                App.getInstance().getExecutor().submit(() -> {
+                    File target = new File(req.getSavePath() + File.separator + req.getFileName() + "." + req.getExtension());
+                    try {
+                        RequestBuilder requestBuilder = RequestBuilder.get(req.getUrl()).setHeader("User-Agent", App.USER_AGENT);
+                        httpClient.execute(requestBuilder.build(), (ResponseHandler<Void>) response -> {
+                            HttpEntity entity = response.getEntity();
+                            long contentLength = entity.getContentLength();
+                            InputStream is = entity.getContent();
+                            byte[] buf = new byte[1024];
+                            long readSize = 0;
+                            int size;
+                            FileOutputStream fos = new FileOutputStream(target);
+                            List<Long> send = new ArrayList<>();
+                            while ((size = is.read(buf)) != -1) {
+                                fos.write(buf, 0, size);
+                                readSize = readSize + size;
+                                Long progress = ((readSize * 100) / contentLength);
+                                if (send.contains(progress)) {
+                                    continue;
+                                }
+                                send.add(progress);
+                                log.info("{} : {}", req.getFileName(), progress);
+                                if (progress % 2 == 0) {
+                                    DOWNLOADING_MAP.put(req.getDownloadId(), progress.intValue());
+                                }
+                            }
+                            DOWNLOADING_MAP.put(req.getDownloadId(), 100);
+                            fos.flush();
+                            fos.close();
+                            return null;
+                        });
+                    } catch (IOException e) {
+                        log.error(e.getMessage(), e);
+                        DOWNLOADING_MAP.put(req.getDownloadId(), -1);
+                        FileUtils.deleteQuietly(target);
+                    }
+                });
+            }
             callback.success("downloading");
         });
     }
--- src/main/resources/static/src/components/ResourceList.jsx
+++ src/main/resources/static/src/components/ResourceList.jsx
@@ -1,9 +1,23 @@
 import React from "react";
-import {AppBar, Grid, MenuItem, Select, Toolbar, withStyles} from "@material-ui/core";
+import {
+    Container,
+    Divider,
+    IconButton,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+    withStyles
+} from "@material-ui/core";
 import axios from "axios";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import Link from "@material-ui/core/Link";
-import {Pagination} from "@material-ui/lab";
+import Chip from '@material-ui/core/Chip';
+import FolderIcon from '@material-ui/icons/Folder';
+import GetAppIcon from "@material-ui/icons/GetApp";
 
 const resourceListUrl = "https://coolapi.coolcollege.cn/enterprise-api/course/resource/list?keyword={keyword}&parent_id={parentId}&resource_classify={resourceClassify}&page_number={pageIndex}&page_size={pageSize}&course_flag={courseFlag}&review={review}&access_token={token}";
 
@@ -14,6 +28,13 @@ const styles = theme => ({
     },
 });
 
+const StyledBreadcrumb = withStyles((theme) => ({
+    root: {
+        height: theme.spacing(3),
+        fontWeight: theme.typography.fontWeightRegular
+    },
+}))(Chip);
+
 class ResourceList extends React.Component {
 
     constructor(props) {
@@ -22,12 +43,14 @@ class ResourceList extends React.Component {
             keyword: '',
             parentId: 0,
             resourceClassify: 0,
-            pageIndex: 1,
+            pageIndex: 0,
             pageSize: 20,
             totalPage: 0,
+            totalRows: 0,
             courseFlag: 1,
             review: 'block',
-            items: []
+            items: [],
+            breadcrumbs: []
         }
     }
 
@@ -40,7 +63,7 @@ class ResourceList extends React.Component {
             keyword: this.state.keyword,
             parentId: this.state.parentId,
             resourceClassify: this.state.resourceClassify,
-            pageIndex: this.state.pageIndex,
+            pageIndex: this.state.pageIndex + 1,
             pageSize: this.state.pageSize,
             courseFlag: this.state.courseFlag,
             review: this.state.review,
@@ -50,7 +73,8 @@ class ResourceList extends React.Component {
             console.log(data);
             this.setState({
                 totalPage: data['pages'],
-                pageIndex: data['page_num'],
+                totalRows: data['total'],
+                pageIndex: data['page_num'] - 1,
                 items: data['list']
             }, () => {
                 console.log(this.state);
@@ -61,59 +85,151 @@ class ResourceList extends React.Component {
     render() {
         const classes = this.props.classes;
         return <React.Fragment>
-            <Breadcrumbs aria-label="breadcrumb">
-                <Link color="inherit" href="#" onClick={() => {
-                    this.setState({
-                        parentId: 0,
-                        resourceClassify: 0,
-                        pageIndex: 1
-                    }, () => {
-                        this.fetchResourceList();
-                    })
-                }}>
-                    公共区域
-                </Link>
-            </Breadcrumbs>
-
-            <AppBar position="fixed" color="default" className={classes.bottomAppBar}>
-                <Toolbar>
-                    <Grid container spacing={4}>
-                        <Grid item>
-
-                        </Grid>
-                        <Grid item>
-                            <Select
-                                style={{width: 100}}
-                                labelId="page-size-2"
-                                id="page-size-2"
-                                value={this.state.pageSize}
-                                onChange={e => {
+            <Container>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <StyledBreadcrumb
+                        key={'bread--'}
+                        component="a"
+                        href="#"
+                        label="公共区"
+                        icon={<FolderIcon fontSize="small"/>}
+                        onClick={() => {
+                            this.setState({
+                                parentId: 0,
+                                resourceClassify: 0,
+                                pageIndex: 0,
+                                breadcrumbs: []
+                            }, () => {
+                                this.fetchResourceList();
+                            })
+                        }}
+                    />
+                    {
+                        this.state.breadcrumbs.map((item, index) => (
+                            <StyledBreadcrumb
+                                key={'bread-' + index}
+                                component="a"
+                                href="#"
+                                label={item['name']}
+                                icon={<FolderIcon fontSize="small"/>}
+                                onClick={() => {
+                                    let breadcrumbs = this.state.breadcrumbs;
+                                    let index = 0;
+                                    for (let i = 0; i < breadcrumbs.length; i ++) {
+                                        if (breadcrumbs[i]['id'] === item['id']) {
+                                            index = i + 1;
+                                            break;
+                                        }
+                                    }
+                                    console.log(index);
+                                    let newBreadcrumbs = 0 === index ? [] : breadcrumbs.slice(0, index);
                                     this.setState({
-                                        pageSize: e.target.value
+                                        parentId: item['id'],
+                                        resourceClassify: item['id'],
+                                        pageIndex: 0,
+                                        breadcrumbs: newBreadcrumbs
                                     }, () => {
                                         this.fetchResourceList();
                                     })
                                 }}
-                            >
-                                <MenuItem value={20}>20条/页</MenuItem>
-                                <MenuItem value={30}>30条/页</MenuItem>
-                                <MenuItem value={40}>40条/页</MenuItem>
-                                <MenuItem value={50}>50条/页</MenuItem>
-                            </Select>
-                        </Grid>
-                        <Grid item>
-                            <Pagination count={this.state.totalPage} page={this.state.pageIndex} color="primary"
-                                        onChange={(event, page) => {
-                                            this.setState({
-                                                pageIndex: page
-                                            }, () => {
-                                                this.fetchResourceList();
-                                            })
-                                        }}/>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </AppBar>
+                            />
+                        ))
+                    }
+                </Breadcrumbs>
+                <Divider style={{marginTop: 10}}/>
+                <Paper>
+                    <TableContainer style={{maxHeight: '490px'}}>
+                        <Table aria-label="resource table" stickyHeader>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">index</TableCell>
+                                    <TableCell align="center">文件名</TableCell>
+                                    <TableCell align="center">创建时间</TableCell>
+                                    <TableCell align="center">类型</TableCell>
+                                    <TableCell align="center">操作</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    this.state.items.map((item, index) => (
+                                        item['type'] === 'resource' ?
+                                            <TableRow hover key={'detail-item-' + index}>
+                                                <TableCell component="th" scope="row" align="center">
+                                                    {index + 1}
+                                                </TableCell>
+                                                <TableCell component="th" scope="row" align="center">
+                                                    {item['resource']['actualName']}
+                                                </TableCell>
+                                                <TableCell
+                                                    align="center">{new Date(item['resource']['createTime']).format('yyyy-MM-dd hh:mm:ss')}</TableCell>
+                                                <TableCell
+                                                    align="center">{item['resource']['type']}</TableCell>
+                                                <TableCell align="center">
+                                                    <IconButton edge="end" aria-label="delete">
+                                                        <GetAppIcon/>
+                                                    </IconButton>
+                                                </TableCell>
+                                            </TableRow>
+                                            :
+                                            <TableRow hover style={{cursor: 'pointer'}} key={'detail-item-' + index}
+                                                      onClick={e => {
+                                                          e.preventDefault();
+                                                          let breadcrumbs = this.state.breadcrumbs;
+                                                          breadcrumbs.push({
+                                                              name: item['resource']['name'],
+                                                              id: item['resource']['id']
+                                                          })
+                                                          this.setState({
+                                                              parentId: item['resource']['id'],
+                                                              resourceClassify: item['resource']['id'],
+                                                              breadcrumbs: breadcrumbs
+                                                          }, () => {
+                                                              this.fetchResourceList();
+                                                          })
+                                                      }}>
+                                                <TableCell component="th" scope="row" align="center">
+                                                    {index + 1}
+                                                </TableCell>
+                                                <TableCell component="th" scope="row" align="center">
+                                                    {item['resource']['name']}
+                                                </TableCell>
+                                                <TableCell
+                                                    align="center">{new Date(item['resource']['createTime']).format('yyyy-MM-dd hh:mm:ss')}</TableCell>
+                                                <TableCell component="th" scope="row" align="center">
+                                                    -
+                                                </TableCell>
+                                                <TableCell align="center">{""}</TableCell>
+                                            </TableRow>
+
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[20, 30, 40, 50]}
+                        component="div"
+                        count={this.state.totalRows}
+                        rowsPerPage={this.state.pageSize}
+                        page={this.state.pageIndex}
+                        onPageChange={(event, page) => {
+                            console.log(page);
+                            this.setState({
+                                pageIndex: page
+                            }, () => {
+                                this.fetchResourceList();
+                            })
+                        }}
+                        onRowsPerPageChange={e => {
+                            this.setState({
+                                pageSize: e.target.value
+                            }, () => {
+                                this.fetchResourceList();
+                            })
+                        }}
+                    />
+                </Paper>
+            </Container>
         </React.Fragment>
     }
 }
